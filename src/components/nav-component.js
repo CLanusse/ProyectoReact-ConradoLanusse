2ddb469fd010785e21a2aa811db9e6518cb2c81e
Conrado Lanusse <conrado.lanusse@gmail.com>
2021-03-10 09:38:39 -0300

    agregado NavComponent y estilos

---
+++ src/components/nav-component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavComponent = ()=>{
+    return <header className="header">
+                <h1>Logo E-Commerce</h1>
+
+                <nav>
+                    <a href="#">Nosotros</a>
+                    <a href="#">Productos</a>
+                    <a href="#">Contacto</a>
+                </nav>
+            </header>
+    
+}
+
+export default NavComponent;
